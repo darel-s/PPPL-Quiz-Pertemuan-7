a2df579e6a3d8bf79abf01523d41cb5ab4bd66c6
Darriel Markerizal <darrielmarkerizal@gmail.com>
2024-03-27 12:48:54 +0700

    finish

--- src/test/java/BankUnitTest.java
+++ src/test/java/BankUnitTest.java
@@ -1,2 +1,82 @@
+import org.example.BankAccount;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BankUnitTest {
+    @BeforeAll
+    public static void initBankAccount() {
+        BankAccount bankAccount = new BankAccount("374245455400126", 10000);
+    }
+
+    @Test
+    public void testGetAccountNumber() {
+        BankAccount bankAccount = new BankAccount("374245455400126", 10000);
+        assertEquals("374245455400126", bankAccount.getAccountNumber());
+    }
+
+    @Test
+    public void testGetAccountNumber2() {
+        BankAccount bankAccount = new BankAccount("374245455400126", 10000);
+        assertEquals("12345678", bankAccount.getAccountNumber());
+    }
+
+    @Test
+    public void testGetBalance() {
+        BankAccount bankAccount = new BankAccount("374245455400", 10000000);
+        assertEquals(10000000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testGetBalance2() {
+        BankAccount bankAccount = new BankAccount("374245455400", 10000000);
+        assertEquals(1500000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testDeposit() {
+        BankAccount bankAccount = new BankAccount("374245455400126", 1500000);
+        bankAccount.deposit(500000);
+        assertEquals(2000000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testDeposit2() {
+        BankAccount bankAccount = new BankAccount("374245455400126", 1500000);
+        bankAccount.deposit(500000);
+        assertEquals(2500000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testWithdraw() {
+        BankAccount bankAccount = new BankAccount("374245455400126", 2500000);
+        bankAccount.withdraw(500000);
+        assertEquals(2000000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testWithdraw2() {
+        BankAccount bankAccount = new BankAccount("374245455400126", 2500000);
+        bankAccount.withdraw(500000);
+        assertEquals(3000000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testTransferFunds() {
+        BankAccount bankAccount = new BankAccount("374245455400", 10000000);
+        BankAccount recipient = new BankAccount("6034932528973614", 10000000);
+        bankAccount.transferFunds(recipient, 5000000);
+        assertEquals(5000000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testTransferFunds2() {
+        BankAccount bankAccount = new BankAccount("374245455400", 10000000);
+        BankAccount recipient = new BankAccount("6034932528973614", 10000000);
+        bankAccount.transferFunds(recipient, 5000000);
+        assertEquals(15000000, bankAccount.getBalance());
+    }
 }
--- src/test/java/DateUnitTest.java
+++ src/test/java/DateUnitTest.java
@@ -1,3 +1,35 @@
-public class DateUnitTest {
+import org.example.DateUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+public class DateUnitTest {
+    @Test
+    public void testIsLeapYear() {
+        DateUtils dateUtils = new DateUtils();
+        boolean result = dateUtils.isLeapYear(2020);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsLeapYear2() {
+        DateUtils dateUtils = new DateUtils();
+        boolean result = dateUtils.isLeapYear(2019);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testGetDaysInMonth() {
+        DateUtils dateUtils = new DateUtils();
+        int result = dateUtils.getDaysInMonth(2020, 2);
+        assertTrue(result == 29);
+    }
+
+    @Test
+    public void testGetDaysInMonth2() {
+        DateUtils dateUtils = new DateUtils();
+        int result = dateUtils.getDaysInMonth(2020, 3);
+        assertTrue(result == 29);
+    }
 }
